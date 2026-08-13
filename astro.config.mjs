a7b929ff33a333d0bd7e4b52f0dd834f01ee30ca
Ahmed Sheta <a.a.shettaa@gmail.com>
2026-08-13 15:38:48 -0700

    Use blog.sheta.dev as canonical domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://sheta.dev',
+  site: 'https://blog.sheta.dev',
   output: 'static',
   trailingSlash: 'never',
   build: {
